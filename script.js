84882586228e1129c682d375a9fb80a260630de8
LubenStoyanov <luben.stoyanov.ls@gmail.com>
2022-09-13 15:43:21 +0200

    Change to simple task list and restructure script.js

--- script.js
+++ script.js
@@ -2,13 +2,39 @@ const taskList = document.querySelector("#task-list");
 const taskName = document.querySelector("#task-name");
 const taskArray = [];
 
+const createDiv = (task) => {
+  const taskDiv = document.createElement("div");
+  taskList.appendChild(taskDiv);
+  addTaskToList(taskDiv, task);
+};
+
+const addTaskToList = (taskDiv, task) => {
+  taskDiv.innerHTML += `<input type="checkbox" name="task" id="task"/>
+    <label for="${task}">${task}</label>
+    <button id="btn-${task}">X</button>
+    `;
+};
+
+const addTask = () => {
+  const task = taskName.value;
+  createDiv(task);
+  taskArray.push(task);
+  taskName.value = "";
+};
+
 taskName.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
-    const task = taskName.value;
-    taskArray.push(task);
-    taskList.innerHTML += `<li>${task}</li>`;
-    taskName.value = "";
+    addTask();
   }
 });
 
 // renderTasks(e.target.id.split("-")[0]);
+
+window.addEventListener("unload", function (event) {
+  //when the window is closed
+  localStorage.setItem("pickColor", "meep"); // save selected color to localStorage
+  let sending = browser.runtime.sendMessage({
+    message: "color set done",
+  });
+  console.log("message sent");
+});
